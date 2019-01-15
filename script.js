b4b5516e67703b3f8c7c4c2077c68109e9b4c841
Spectre <hugo.boucheron@epitech.eu>
2019-01-15 17:36:37 +0100

    first commit, start of the project

---
+++ script.js
@@ -0,0 +1,6 @@
+function launch_game(test) {
+    document.getElementById("menu").style.display ="none";
+    document.getElementById("game").style.display ="flex";
+    var x = document.getElementsByClassName("player")[0].id;
+    console.log(x);
+}
