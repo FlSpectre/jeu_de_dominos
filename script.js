1c1edb593704f0d8d0284786d0c2cf76494e2ee6
Spectre <hugo.boucheron@epitech.eu>
2019-01-16 19:58:36 +0100

    few changes added

--- script.js
+++ script.js
@@ -39,21 +39,57 @@ function end_turn() {
     }
     document.getElementById("p1").style.display ="flex";
 }
-/*
-    if (turn == 2 && (x == "3" || x == "4"))
-        var player = 1;
-        turn++;
-        if (x == "3") {
-            turn = 0;
-        }
 
-    }
-    if (turn == 3 && x == "4")
-        var player = 1;
-        turn = 0;
+function create_dominos() {
+    var dominos = ["0","1","2","3","4","5","6","7","8","9","10",
+    "11","12","13","14","15","16","17","18","19","20","21","22",
+    "23","24","25","26","27"];
+    return dominos;
+}
+
+function get_player_hand() {
+    var array = create_dominos();
+    new_array = randomize(array);
+
+    var new_array;
+    var hand1 = [];
+    var hand2 = [];
+    var i = 0;
+
+    while (i != 7) {
+        hand1[i] = array[i];
+        array.splice(i, 1);
+        i++;
+    } 
+    console.log(hand1);
+    console.log(array);
+    aff_hand(hand1);
+}
+
+function aff_hand(hand1) {
+    var i = hand1.length;
+    var test;
+    while (i !== 0) {
+        test = document.getElementsByClassName("Dominos")[hand1[0]];
+    console.log(test);
+
+        document.getElementById("d1").style.display = "none";
+        i--;
     }
 }
-*/
+
+function randomize(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
 
 function create_map() {
     
